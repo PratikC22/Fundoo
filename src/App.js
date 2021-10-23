5855031017b1f928d87200cb28bf2b81b452ad1f
Pratik Chaudhari <pratikc1020@gmail.com>
2021-10-23 17:57:24 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Dashboard from './components/dashboard/dashboard';
+import ViewNotes from "./components/viewNotes/viewNotes";
+
+import RouterDom from "./router/router";
+
+function App() {
+  return (
+    <div>
+      {/* <RouterDom /> */}
+      <Dashboard />
+      {/* <ViewNotes /> */}
+    </div>
+  );
+}
+
+export default App;
